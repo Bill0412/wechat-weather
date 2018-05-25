fb55dd3e9cf879f45649897224119abb85d1ccb5
Bill Xu <jxphxufh@gmail.com>
2018-05-25 09:57:44 +0800

    Update the temperature and weather data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,12 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
 Page({
   data:{
     nowTemp: 14,
@@ -7,13 +16,16 @@ Page({
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city:'广州市'
+        city:'北京市'
       },
       success: res => {
         let result = res.data.result
         let temp = result.now.temp
         let weather = result.now.weather
-        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        })
       }
     })
   }
